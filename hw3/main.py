a5aa2ac2ba34e102e9d35bbdc92a60413a59cb21
tretikoff <Konstantin.Tretiakov@jetbrains.com>
2022-02-14 17:53:37 +0300

    hw3

--- hw3/main.py
+++ hw3/main.py
@@ -1,11 +1,6 @@
 import numpy as np
 
 
-class MatrixHashMixin:
-    def __hash__(self):
-        pass
-
-
 class MatrixStrMixin:
     def __init__(self, value):
         self.value = value
@@ -28,35 +23,62 @@ class MatrixStrMixin:
             result += " ".join((map(str, self.value[h]))) + "\n"
         return result
 
+    def __eq__(self, other):
+        if self.height != other.height or self.width != other.width: return False
+        for h in range(self.height):
+            for w in range(self.width):
+                if self.value[h] != self.value[w]: return False
+        return True
 
-class MatrixEasy(MatrixHashMixin, MatrixStrMixin):
+    # Hash of all matrix elements [a11 a12 ... anm]
+    # of type (a11 + 2*b12 + 3*b13 +..+((n+1)*(m+1)*bnm)) % 20
+    # where bxy = axy % 20
+    def __hash__(self):
+        res = 0
+        for h in range(self.height):
+            for w in range(self.width):
+                res += (int(self.value[h][w]) % 20) * (h * w + w + 1)
+        return res % 20
+
+
+class MatrixEasy(MatrixStrMixin):
+    cache = {}
+    multiplied = False
+
     def __matmul__(self, other):
+        h1 = hash(self)
+        h2 = hash(other)
+        if self.cache.get(2):
+            return self.cache[h2]
+
         if self.width != other.height:
             raise ArithmeticError("Matrix sizes do not match")
-        result = [[None] * other.width] * self.height
+        result = MatrixEasy([[None] * other.width] * self.height)
         for i in range(self.height):
             for j in range(other.width):
                 for k in range(other.height):
-                    result[i][j] = self.value[i][k] * other.value[k][j]
-        return MatrixEasy(result)
+                    result.value[i][j] = self.value[i][k] * other.value[k][j]
+        self.cache[h2] = result
+        self.multiplied = True
+        return result
 
     def __mul__(self, other):
         if self.height != other.height or self.width != other.width:
             raise ArithmeticError("Matrix sizes do not match")
-        result = [[None] * self.width] * self.height
+        result = MatrixEasy([[None] * self.width] * self.height)
         for h in range(self.height):
             for w in range(self.width):
-                result[h][w] = self.value[h][w] * other.value[h][w]
-        return MatrixEasy(result)
+                result.value[h][w] = self.value[h][w] * other.value[h][w]
+        return result
 
     def __add__(self, other):
         if self.height != other.height or self.width != other.width:
             raise ArithmeticError("Matrix sizes do not match")
-        result = [[None] * self.width] * self.height
+        result = MatrixEasy([[None] * self.width] * self.height)
         for h in range(self.height):
             for w in range(self.width):
-                result[h][w] = self.value[h][w] + other.value[h][w]
-        return MatrixEasy(result)
+                result.value[h][w] = self.value[h][w] + other.value[h][w]
+        return result
 
 
 class Matrix(MatrixStrMixin, np.lib.mixins.NDArrayOperatorsMixin):
@@ -64,11 +86,10 @@ class Matrix(MatrixStrMixin, np.lib.mixins.NDArrayOperatorsMixin):
         return self.value
 
 
-
 def run_easy():
     m1 = MatrixEasy(np.random.randint(0, 10, (10, 10)))
     m2 = MatrixEasy(np.random.randint(0, 10, (10, 10)))
-    eval("m1 + m2").write_to_file("artifacts/easy/matrix+.txt")
+    eval("m1 + m2").write_to_file("artifacts/easy/A.txt")
     eval("m1 @ m2").write_to_file("artifacts/easy/matrix@.txt")
     eval("m1 * m2").write_to_file("artifacts/easy/matrix_mul.txt")
 
@@ -81,6 +102,34 @@ def run_medium():
     Matrix(eval("m1 * m2")).write_to_file("artifacts/medium/matrix_mul.txt")
 
 
+def run_hard():
+    filename = "artifacts/hard/%s.txt"
+    a = MatrixEasy([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    c = MatrixEasy([[21, 22, 23], [24, 25, 26], [27, 28, 29]])
+    assert hash(a) == hash(c)
+    assert a != c
+    d = MatrixEasy(np.random.randint(0, 10, (3, 3)))
+    b = d
+
+    a * b
+    a.multiplied = False
+    ab = a * b
+    assert not a.multiplied
+
+    cd = c * d
+
+    a.write_to_file(filename % "A")
+    b.write_to_file(filename % "B")
+    c.write_to_file(filename % "C")
+    d.write_to_file(filename % "D")
+    ab.write_to_file(filename % "AB")
+    cd.write_to_file(filename % "CD")
+
+    MatrixEasy([[hash(ab), hash(cd)]]).write_to_file(filename % "hash")
+    assert a != c
+
+
 if __name__ == '__main__':
     run_easy()
     run_medium()
+    run_hard()
