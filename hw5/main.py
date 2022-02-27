fd8d98054d868222f0b96dad51323f33d41ee019
tretikoff <Konstantin.Tretiakov@jetbrains.com>
2022-02-27 23:19:57 +0300

    hw 5

--- hw5/main.py
+++ hw5/main.py
@@ -1,12 +1,20 @@
 # Асинхронное скачивание картинок\файлов с любого сайта из https://thisxdoesnotexist.com/
 # Сделать питоновский скрипт, которому можно указать количество файлов,
 # которые нужно получить в нужную папку. Использовать aiohttp.
-url = "https://thisxdoesnotexist.com/"
+import asyncio
+import sys
+import aiohttp
+
+url = "https://thiscatdoesnotexist.com/"
 
 
-def run_easy():
-    pass
+async def run_easy():
+    count = int(sys.argv[0])
+    async with aiohttp.ClientSession() as session:
+        for i in range(count):
+            async with session.get(url) as response:
+                with open(f"artifacts/easy/{i}.png", "bw") as file:
+                    file.write((await response.content.read()))
 
-
-if __name__ == '__main__':
-    run_easy()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run_easy())
