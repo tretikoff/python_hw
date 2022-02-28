da9a9edd1c5fd42ca81663091f3e55da167e6551
tretikoff <Konstantin.Tretiakov@jetbrains.com>
2022-02-28 08:49:19 +0300

    hw 5

--- hw5/main.py
+++ hw5/main.py
@@ -3,18 +3,23 @@
 # которые нужно получить в нужную папку. Использовать aiohttp.
 import asyncio
 import sys
+import time
+
 import aiohttp
 
 url = "https://thiscatdoesnotexist.com/"
 
 
 async def run_easy():
-    count = int(sys.argv[0])
+    count = int(sys.argv[1])
     async with aiohttp.ClientSession() as session:
         for i in range(count):
             async with session.get(url) as response:
                 with open(f"artifacts/easy/{i}.png", "bw") as file:
                     file.write((await response.content.read()))
+            # Images on the site are updated once every second.
+            # Could be removed, then adjacent images sometimes will be repeated
+            time.sleep(1)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run_easy())
